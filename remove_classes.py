8a7334e034d6adb8b1ff19e8b69b106135a3d578
Nikita Sokovnin <nikitoos67@gmail.com>
2024-04-24 22:24:33 +0200

    Improve remove classes script

--- remove_classes.py
+++ remove_classes.py
@@ -37,6 +37,8 @@ def remove_classes(input_dir, output_dir, percentage):
     # Create dir for removed classes
     os.makedirs(os.path.join(output_dir, "unknown_classes"))
 
+    os.makedirs(os.path.join(output_dir, "unknown_classes_test"))
+
     # Copy the remaining training classes to the output directory
     for class_dir in train_class_dirs:
         if class_dir not in classes_to_remove:
@@ -58,7 +60,7 @@ def remove_classes(input_dir, output_dir, percentage):
                 shutil.copytree(os.path.join(test_input_dir, class_dir), destination_dir)
         else:
             # Check if destination directory exists
-            destination_dir = os.path.join(output_dir, "unknown_classes", class_dir)
+            destination_dir = os.path.join(output_dir, "unknown_classes_test", class_dir)
             if os.path.exists(destination_dir):
                 # Append images to the existing destination folder
                 for file in os.listdir(os.path.join(test_input_dir, class_dir)):
@@ -72,10 +74,9 @@ if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_dir", type=str, required=True, help="Path to the input directory")
+    parser.add_argument("--percentage", type=int, required=False, help="Percentage of classes to remove")
     args = parser.parse_args()
     #input_dir = "datasets/tiny-imagenet-200"
-    percentage_to_remove = 10  # Change this value to the desired percentage
+    output_dir = f"{args.input_dir}_{100-args.percentage}"
 
-    output_dir = f"{args.input_dir}_{100-percentage_to_remove}"
-
-    remove_classes(args.input_dir, output_dir, percentage_to_remove)
+    remove_classes(args.input_dir, output_dir, args.percentage)
